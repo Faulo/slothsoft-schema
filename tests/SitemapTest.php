fcac54fbeaf7ebd8a3dfe7d50cad5508053db09a
Faulo <FauloLio@gmail.com>
2018-04-03 18:26:48 +0200

    sitemap

---
+++ tests/SitemapTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace tests;
+
+use Slothsoft\Farah\Configuration\AssetConfigurationField;
+use Slothsoft\Farah\ModuleTests\AbstractSitemapTest;
+use Slothsoft\Farah\Module\Node\Asset\AssetInterface;
+
+class SitemapTest extends AbstractSitemapTest
+{
+    protected static function loadSitesAsset() : AssetInterface {
+        return (new AssetConfigurationField('farah://slothsoft@schema/sitemap/schema.slothsoft.net'))->getValue();
+    }
+}
