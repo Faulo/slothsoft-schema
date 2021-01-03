6fdcd25ae1ccbbc133f4bb2c51036cce15eba62e
Daniel Schulz <faulolio@gmail.com>
2021-01-03 20:31:12 +0100

    Proper version sorting

--- src/Assets/ManifestBuilder.php
+++ src/Assets/ManifestBuilder.php
@@ -3,9 +3,7 @@ declare(strict_types = 1);
 namespace Slothsoft\Schema\Assets;
 
 use Slothsoft\Core\IO\Writable\Delegates\DOMWriterFromElementDelegate;
-use Slothsoft\Farah\FarahUrl\FarahUrl;
 use Slothsoft\Farah\FarahUrl\FarahUrlArguments;
-use Slothsoft\Farah\Module\Module;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 use Slothsoft\Farah\Module\Asset\ExecutableBuilderStrategy\ExecutableBuilderStrategyInterface;
 use Slothsoft\Farah\Module\Executable\ExecutableStrategies;
@@ -19,19 +17,7 @@ class ManifestBuilder implements ExecutableBuilderStrategyInterface {
 
     public function buildExecutableStrategies(AssetInterface $context, FarahUrlArguments $args): ExecutableStrategies {
         if ($schemaId = $args->get('schema')) {
-            if ($version = $args->get('version')) {
-                $schemaId .= "/$version";
-                $schemaUrl = FarahUrl::createFromReference($schemaId);
-                $schemaVersions = [
-                    $schemaUrl
-                ];
-            } else {
-                $schemaUrl = FarahUrl::createFromReference($schemaId);
-                $schemaVersions = [];
-                foreach (Module::resolveToAsset($schemaUrl)->getAssetChildren() as $asset) {
-                    $schemaVersions[] = $asset->createUrl();
-                }
-            }
+            $schemaVersions = SchemaParameterFilter::createSchemaVersions($schemaId, $args->get('version'));
             $closure = function (DOMDocument $targetDoc) use ($schemaVersions): DOMElement {
                 $rootNode = $targetDoc->createElement('schema-manifest');
                 foreach ($schemaVersions as $url) {
